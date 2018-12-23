8f36e4185cd7c671df9d298e93e8f7b51472c503
Hermit <yzy627@126.com>
2018-12-23 14:59:40 +0800

    init development env

---
+++ src/test/java/edu/nju/usm/UsmApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.nju.usm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
