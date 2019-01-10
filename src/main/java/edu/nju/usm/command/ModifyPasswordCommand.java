3900e621c925657042198016669a150dc6427499
sx14 <1059363093@qq.com>
2019-01-10 22:23:18 +0800

    hello ha ~~~ --sunx

--- src/main/java/edu/nju/usm/command/ModifyPasswordCommand.java
+++ src/main/java/edu/nju/usm/command/ModifyPasswordCommand.java
@@ -17,4 +17,20 @@ public class ModifyPasswordCommand {
     private String oldPassword;
     private String newPassword;
 
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
